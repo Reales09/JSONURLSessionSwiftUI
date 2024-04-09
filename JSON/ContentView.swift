69698ae42828af232633e24dc348b7ade74a8e9f
Reales Myles <rclarence123@gmail.com>
2024-04-09 01:18:13 -0500

    URLSessions

--- JSON/ContentView.swift
+++ JSON/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var login = PostViewModel()
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ JSON/Model/PostModel.swift
@@ -0,0 +1,13 @@
+//
+//  PostModel.swift
+//  JSON
+//
+//  Created by Reales Rectoro Myles Clarence on 9/04/24.
+//
+
+import Foundation
+
+struct PostModel: Decodable{
+    var token : String
+    
+}
