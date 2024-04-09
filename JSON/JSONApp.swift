8d36c7c5b760575cba2c03a1476b43b26a873fb5
Reales Myles <rclarence123@gmail.com>
2024-04-09 00:35:54 -0500

    Initial Commit

---
+++ JSON/JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONApp.swift
+//  JSON
+//
+//  Created by Reales Rectoro Myles Clarence on 8/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
