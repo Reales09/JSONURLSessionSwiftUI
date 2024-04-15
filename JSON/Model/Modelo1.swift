6fbc917d51df56f04fe98d11878c4e3e0c3dab13
Reales Myles <rclarence123@gmail.com>
2024-04-14 23:49:21 -0500

    Modelo obtener json

---
+++ JSON/Model/Modelo1.swift
@@ -0,0 +1,15 @@
+//
+//  Modelo1.swift
+//  JSON
+//
+//  Created by Reales Rectoro Myles Clarence on 14/04/24.
+//
+
+import Foundation
+
+struct Modelo1: Decodable{
+    var id : Int
+    var name : String
+    var email : String
+    
+}
